fcb5b86f539e389c6785dbdaceaa66cf8075587b
pid011 <pid011@outlook.com>
2025-12-29 17:58:23 +0900

    사용하지 않는 테라폼 코드 제거

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -1,11 +1,8 @@
 
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "lionpay-terraform-state"
-
-  lifecycle {
-    prevent_destroy = true
-  }
+  bucket        = "lionpay-terraform-state"
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
--- terraform/ecr/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "lionpay-terraform-state"
-    key            = "ecr/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-    dynamodb_table = "terraform-locks"
-  }
-}
