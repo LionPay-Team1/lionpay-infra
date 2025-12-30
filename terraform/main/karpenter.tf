241e4807073b695fd888f5a2f81af1f16b085fd6
pid011 <pid011@outlook.com>
2025-12-30 12:50:24 +0900

    카펜터 오류 수정 및 설정 변경 (#13)

--- terraform/main/karpenter.tf
+++ terraform/main/karpenter.tf
@@ -37,3 +37,73 @@ module "karpenter_tokyo" {
 
   tags = local.tags
 }
+
+###############################################################
+# Karpenter Config Automation
+###############################################################
+
+resource "local_file" "karpenter_seoul_manifest" {
+  content = templatefile("${path.module}/config/${var.env}-karpenter.yaml", {
+    cluster_name          = module.eks_seoul.cluster_name
+    environment           = var.env
+    instance_profile_name = module.eks_seoul.karpenter_instance_profile_name
+  })
+  filename = "${path.module}/.terraform/karpenter_seoul.yaml"
+}
+
+resource "null_resource" "karpenter_seoul_apply" {
+  triggers = {
+    manifest_sha1 = sha1(local_file.karpenter_seoul_manifest.content)
+  }
+
+  # 1. Update Kubeconfig
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name ${module.eks_seoul.cluster_name} --region ap-northeast-2"
+    environment = {
+      KUBECONFIG = "${path.module}/.terraform/kubeconfig_seoul"
+    }
+  }
+
+  # 2. Apply Manifest
+  provisioner "local-exec" {
+    command = "kubectl apply -f ${local_file.karpenter_seoul_manifest.filename}"
+    environment = {
+      KUBECONFIG = "${path.module}/.terraform/kubeconfig_seoul"
+    }
+  }
+
+  depends_on = [module.karpenter_seoul]
+}
+
+resource "local_file" "karpenter_tokyo_manifest" {
+  content = templatefile("${path.module}/config/${var.env}-karpenter.yaml", {
+    cluster_name          = module.eks_tokyo.cluster_name
+    environment           = var.env
+    instance_profile_name = module.eks_tokyo.karpenter_instance_profile_name
+  })
+  filename = "${path.module}/.terraform/karpenter_tokyo.yaml"
+}
+
+resource "null_resource" "karpenter_tokyo_apply" {
+  triggers = {
+    manifest_sha1 = sha1(local_file.karpenter_tokyo_manifest.content)
+  }
+
+  # 1. Update Kubeconfig
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name ${module.eks_tokyo.cluster_name} --region ap-northeast-1"
+    environment = {
+      KUBECONFIG = "${path.module}/.terraform/kubeconfig_tokyo"
+    }
+  }
+
+  # 2. Apply Manifest
+  provisioner "local-exec" {
+    command = "kubectl apply -f ${local_file.karpenter_tokyo_manifest.filename}"
+    environment = {
+      KUBECONFIG = "${path.module}/.terraform/kubeconfig_tokyo"
+    }
+  }
+
+  depends_on = [module.karpenter_tokyo]
+}
