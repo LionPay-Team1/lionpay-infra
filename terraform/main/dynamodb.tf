21c6239e98ae2b0cedd0b6becd071dd99dc08d8a
pid011 <pid011@outlook.com>
2026-01-05 17:11:37 +0900

    configmap, secret 최신화 및 구성

--- terraform/main/dynamodb.tf
+++ terraform/main/dynamodb.tf
@@ -14,5 +14,19 @@ module "dynamodb" {
   point_in_time_recovery = false
   deletion_protection    = false
   replica_regions        = [{ region_name = "ap-northeast-1" }]
-  tags                   = local.tags
+
+  # CloudFormation 로컬 개발 환경과 동일하게 GSI 추가
+  global_secondary_indexes = [
+    {
+      name            = "byRefreshToken"
+      hash_key        = "token"
+      hash_key_type   = "S"
+      projection_type = "ALL"
+    }
+  ]
+
+  # Server-Side Encryption 활성화
+  server_side_encryption = true
+
+  tags = local.tags
 }
--- terraform/main/k8s_configmaps.tf
+++ terraform/main/k8s_configmaps.tf
@@ -3,32 +3,82 @@
 ###############################################################
 
 # Seoul Cluster (Hub)
-resource "kubernetes_config_map_v1" "global_config_seoul" {
+resource "kubernetes_config_map_v1" "auth_config_seoul" {
   provider = kubernetes.seoul
 
   metadata {
-    name      = "global-config"
+    name      = "auth-config"
     namespace = "default"
   }
 
   data = {
-    "AWS_REGION" = "ap-northeast-2"
+    "AWS_REGION"                  = "ap-northeast-2"
+    "DYNAMODB_TABLE_NAME"         = var.dynamodb_table_name
+    "JWT_ISSUER"                  = var.jwt_issuer
+    "JWT_AUDIENCES"               = var.jwt_audiences
+    "OTEL_EXPORTER_OTLP_ENDPOINT" = "http://alloy.monitoring.svc.cluster.local:4317"
+    "OTEL_SERVICE_NAME"           = "lionpay-auth"
+  }
+
+  depends_on = [module.eks_seoul]
+}
+
+resource "kubernetes_config_map_v1" "wallet_config_seoul" {
+  provider = kubernetes.seoul
+
+  metadata {
+    name      = "wallet-config"
+    namespace = "default"
+  }
+
+  data = {
+    "AWS_REGION"                  = "ap-northeast-2"
+    "WALLETDB_CLUSTER_ENDPOINT"   = "${module.dsql_seoul.identifier}.dsql.ap-northeast-2.on.aws"
+    "JWT_ISSUER"                  = var.jwt_issuer
+    "JWT_AUDIENCES"               = var.jwt_audiences
+    "OTEL_EXPORTER_OTLP_ENDPOINT" = "http://alloy.monitoring.svc.cluster.local:4317"
+    "OTEL_SERVICE_NAME"           = "lionpay-wallet"
   }
 
   depends_on = [module.eks_seoul]
 }
 
 # Tokyo Cluster (Spoke)
-resource "kubernetes_config_map_v1" "global_config_tokyo" {
+resource "kubernetes_config_map_v1" "auth_config_tokyo" {
   provider = kubernetes.tokyo
 
   metadata {
-    name      = "global-config"
+    name      = "auth-config"
     namespace = "default"
   }
 
   data = {
-    "AWS_REGION" = "ap-northeast-1"
+    "AWS_REGION"                  = "ap-northeast-1"
+    "DYNAMODB_TABLE_NAME"         = var.dynamodb_table_name
+    "JWT_ISSUER"                  = var.jwt_issuer
+    "JWT_AUDIENCES"               = var.jwt_audiences
+    "OTEL_EXPORTER_OTLP_ENDPOINT" = "http://alloy.monitoring.svc.cluster.local:4317"
+    "OTEL_SERVICE_NAME"           = "lionpay-auth"
+  }
+
+  depends_on = [module.eks_tokyo]
+}
+
+resource "kubernetes_config_map_v1" "wallet_config_tokyo" {
+  provider = kubernetes.tokyo
+
+  metadata {
+    name      = "wallet-config"
+    namespace = "default"
+  }
+
+  data = {
+    "AWS_REGION"                  = "ap-northeast-1"
+    "WALLETDB_CLUSTER_ENDPOINT"   = "${module.dsql_tokyo.identifier}.dsql.ap-northeast-1.on.aws"
+    "JWT_ISSUER"                  = var.jwt_issuer
+    "JWT_AUDIENCES"               = var.jwt_audiences
+    "OTEL_EXPORTER_OTLP_ENDPOINT" = "http://alloy.monitoring.svc.cluster.local:4317"
+    "OTEL_SERVICE_NAME"           = "lionpay-wallet"
   }
 
   depends_on = [module.eks_tokyo]
